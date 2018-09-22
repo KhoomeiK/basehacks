b9ddf1bd9d7c464f9ceeb7c2c6600d5c8b14ff3c
Rohan Pandey <rohan@Rohans-MacBook-Pro.local>
2018-09-22 15:13:40 -0700

    agoraPartial

--- src/components/page/theCall.vue
+++ src/components/page/theCall.vue
@@ -7,8 +7,77 @@
 
 <script>
 export default {
-  name: "the call",
+  name: "theCall",
   data() {
+    let client = AgoraRTC.createClient({ mode: "live", codec: "h264" });
+    client.init(
+      // initialize Agora
+      "2fdcc95bd67a45e996a0349e87bf0654",
+      () => {
+        console.log("AgoraRTC client initialized");
+      },
+      err => {
+        console.log("AgoraRTC client init failed", err);
+      }
+    );
+    client.join(
+      // join to channel
+      null,
+      "testChannel",
+      "caller",
+      uid => {
+        console.log("User " + uid + " join channel successfully");
+      },
+      err => {
+        console.log("Join channel failed", err);
+      }
+    );
+    localStream = AgoraRTC.createStream({
+      // create video/audio stream
+      streamID: uid,
+      audio: true,
+      video: true,
+      screen: false
+    });
+
+    localStream.init(
+      // initalize audio/video stream
+      () => {
+        console.log("getUserMedia successfully");
+        localStream.play("agora_local");
+      },
+      err => {
+        console.log("getUserMedia failed", err);
+      }
+    );
+
+    client.publish(localStream, err => {
+      // publish stream to channel
+      console.log("Publish local stream error: " + err);
+    });
+
+    client.on("stream-published", evt => {
+      // occurs after stream publishes
+      console.log("Published local stream successfully");
+    });
+
+    client.on("stream-added", evt => {
+      // subscribes to stream when new stream added to channel
+      var stream = evt.stream;
+      console.log("New stream added: " + stream.getId());
+
+      client.subscribe(stream, err => {
+        console.log("Subscribe stream failed", err);
+      });
+    });
+
+    client.on("stream-subscribed", evt => {
+      // plays stream once subscribed
+      var remoteStream = evt.stream;
+      console.log("Subscribe remote stream successfully: " + stream.getId());
+      stream.play("agora_remote" + stream.getId());
+    });
+
     return {};
   }
 };
