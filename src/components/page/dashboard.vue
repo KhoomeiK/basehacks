8680b0beced5d867997f7a34e8e8d3832ac5cc9c
Hackathon Event <hackathon193@e1z2r4p15.42.us.org>
2018-09-23 01:13:05 -0700

    agoraWorkLit

--- src/components/page/dashboard.vue
+++ src/components/page/dashboard.vue
@@ -18,8 +18,13 @@
       </label>
       <p id="right"><span v-if="!ready">Not</span> Ready</p>
     </div>
-      <div class="rect"></div>
-      <p id="quote">"Motivational Quote BTW"</p>
+    <div class="rect">
+      <div class="video-container">
+        <div id="agora_local" class="video"> </div>
+        <div id="agora_remote" class="video"> </div>
+      </div>
+    </div>
+      <p id="quote" v-if="!ready">"Motivational Quote BTW"</p>
       <h2>Comments</h2>
       <p>"OMG! Elliot Ha has helped me overcome depression and cancer and now I'm a billionaire!"</p>
       <br><br><br>
@@ -47,8 +52,85 @@ export default {
     };
   },
   methods: {
-    toggleReady() {
+    async toggleReady() {
       this.setRead(!this.ready);
+
+      if (this.ready) {
+        console.log("ready!");
+        let client = AgoraRTC.createClient({ mode: "live", codec: "h264" });
+        await client.init(
+          // initialize Agora
+          "2fdcc95bd67a45e996a0349e87bf0654",
+          () => {
+            console.log("AgoraRTC client initialized");
+          },
+          err => {
+            console.log("AgoraRTC client init failed", err);
+          }
+        );
+
+        client.join(
+          // join to channel
+          null,
+          this.mydata.id,
+          0,
+          function(uid) {
+            console.log("User " + uid + " join channel successfully");
+          },
+          function(err) {
+            console.log("Join channel failed", err);
+          }
+        );
+
+        let localStream = AgoraRTC.createStream({
+          // create video/audio stream
+          streamID: 1,
+          audio: true,
+          video: true,
+          screen: false
+        });
+
+        localStream.init(
+          // initalize audio/video stream and play local stream on DOM
+          function() {
+            console.log("getUserMedia successfully");
+            // Use agora_local as ID of the dom element
+            localStream.play("agora_local");
+
+            client.publish(localStream, function(err) {
+              // publish stream to channel
+              console.log("Publish local stream error: " + err);
+            });
+          },
+          function(err) {
+            console.log("getUserMedia failed", err);
+          }
+        );
+
+        client.on("stream-published", function(evt) {
+          // occurs after stream publishes
+          console.log("Published local stream successfully");
+        });
+
+        client.on("stream-added", function(evt) {
+          // subscribes to stream when new stream added to channel
+          var stream = evt.stream;
+          console.log("New stream added: " + stream.getId());
+
+          client.subscribe(stream, function(err) {
+            console.log("Subscribe stream failed", err);
+          });
+        });
+
+        client.on("stream-subscribed", function(evt) {
+          // plays stream once subscribed
+          var remoteStream = evt.stream;
+          console.log(
+            "Subscribe remote stream successfully: " + remoteStream.getId()
+          );
+          remoteStream.play("agora_remote");
+        });
+      }
     },
     setRead(val) {
       let ref = db.collection("users").doc(firebase.auth().currentUser.uid);
@@ -86,5 +168,14 @@ export default {
 <style>
 .call-dashboard {
   text-align: center;
+}
+.video-container {
+  display: grid;
+  grid-template-columns: 50% 50%;
+  padding: 10px;
+}
+.video {
+  display: inline-block;
+  height: 500px;
 }
 </style>
