077b7569235daa56890e0ffdcf9c90482ee93d09
pointfour <lesha.seletskiy@gmail.com>
2018-09-23 04:51:56 -0700

    neet

--- src/components/page/homePage.vue
+++ src/components/page/homePage.vue
@@ -11,6 +11,9 @@
         <h1><strong>No More #WaitingInLine</strong><br>for hotlines</h1>
         <router-link class="btn btn-full" :to="{name:'callRequest'}">Call Now</router-link>
         <router-link class="btn btn-empty" :to="{name:'volunteerSignup'}">Volunteer</router-link>
+        <p>
+          We're connecting teenagers with teenagers to make mental<br> health easier and more accessible than ever.
+        </p>
       </div>
     </header>
     <footerlinks/>
