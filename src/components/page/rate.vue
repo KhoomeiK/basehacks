fe5211b81560fcaa4587898c1b59f34753e090e4
pointfour <lesha.seletskiy@gmail.com>
2018-09-23 03:59:38 -0700

    ree

--- src/components/page/rate.vue
+++ src/components/page/rate.vue
@@ -1,19 +1,46 @@
 <template>
     <div>
-        <h1>RATE</h1>
-        <input type="radio" value=1 v-model="feeling"> I feel better
-        <input type="radio" value=0 v-model="feeling"> I feel the same
-        <input type="radio" value=-1 v-model="feeling"> I feel worse
-        
+      <header id="not-main-page">
+        <router-link :to="{name:'homePage'}"><h1 id="not-main-page-company-name"><strong>We & You</strong></h1></router-link>
+        <div id="not-main-page-nav-bar">
+        <headerlinks/>
+      </div>
+
+    </header>
+      <section id="rate">
+      <h2>How would you rate your service?</h2>
+      <form>
+        <div id="rate-form">
+         <span>
+           <label class="container">I feel better.
+           <input type="radio" name="radio" required value=1 v-model="feeling">
+           <span class="checkmark"></span>
+         </label>
+         <label class="container">I feel the same.
+           <input type="radio" name="radio" value=0 v-model="feeling">
+           <span class="checkmark"></span>
+         </label>
+         <label class="container">I feel worse.
+           <input type="radio" name="radio" value=-1 v-model="feeling">
+           <span class="checkmark"></span>
+         </label>
+       </span>
+        </div>
         <br><br>
-        <textarea placeholder="Comments" v-model="comment"/>
-        <br>
-        <button @click="submit()" > Submit </button>
+        <div id="message-label" class="span-one-of-three"><label for="message">Message</label></div>
+        <div class="span-two-of-three"><textarea id="message" name="message" placeholder="Message" required v-model="comment"></textarea></div>
+        <br><br>
+        <div class="span-two-of-three"><a class="btn btn-full" @click="submit()">Submit</a></div>
+      </form>
+    </section>
+    <footerlinks/>
     </div>
 </template>
 
 <script>
-import firebase from 'firebase';
+import footerlinks from "@/components/misc/footerlinks";
+import headerlinks from "@/components/misc/headerlinks";
+import firebase from "firebase";
 export default {
   name: "rate",
   methods: {
@@ -25,16 +52,25 @@ export default {
       call(this.comment).then(res => {
         let point = Math.round(res.data * 10);
         console.log(point);
-        firebase.firestore().collection("users").doc(this.id).get().then(res => {
-          let points = res.data().points + point + parseInt(this.feeling);
-          let calls = res.data().calls + 1;
-          firebase.firestore().collection("users").doc(this.id).update({
-            points,
-            calls
-            });
-        });
+        firebase
+          .firestore()
+          .collection("users")
+          .doc(this.id)
+          .get()
+          .then(res => {
+            let points = res.data().points + point + parseInt(this.feeling);
+            let calls = res.data().calls + 1;
+            firebase
+              .firestore()
+              .collection("users")
+              .doc(this.id)
+              .update({
+                points,
+                calls
+              });
+          });
       });
-    this.$router.push("/");
+      this.$router.push("/");
     }
   },
   data() {
@@ -43,6 +79,10 @@ export default {
       feeling: null,
       id: this.$route.params.id
     };
+  },
+  components: {
+    footerlinks,
+    headerlinks
   }
 };
 </script>
